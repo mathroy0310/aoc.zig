e0522e942bbb64fa4a73a17ce495e114a629f206
mathroy0310 <mathroy0310@gmail.com>
2025-11-16 21:29:10 -0500

    updated 2024 day6 now is lot faster

--- src/2024/day6.zig
+++ src/2024/day6.zig
@@ -4,56 +4,47 @@ const mem = std.mem;
 input: []const u8,
 allocator: mem.Allocator,
 
-const bufcap: usize = 50;
-
 const Coord = struct { x: usize, y: usize };
 
-fn move(c: Coord, dir: Dir, extent: usize) ?Coord {
-    switch (dir) {
-        .North => {
-            if (c.y > 0) {
-                return .{ .x = c.x, .y = c.y - 1 };
-            }
-        },
-        .East => {
-            if (c.x + 1 < extent) {
-                return .{ .x = c.x + 1, .y = c.y };
-            }
-        },
-        .South => {
-            if (c.y + 1 < extent) {
-                return .{ .x = c.x, .y = c.y + 1 };
-            }
-        },
-        .West => {
-            if (c.x > 0) {
-                return .{ .x = c.x - 1, .y = c.y };
-            }
-        },
+const Dir = enum(u2) {
+    North = 0,
+    East = 1,
+    South = 2,
+    West = 3,
+
+    fn turnRight(self: Dir) Dir {
+        const next: u2 = @truncate((@as(u3, @intFromEnum(self)) + 1) % 4);
+        return @enumFromInt(next);
     }
-    return null;
+};
+
+fn move(c: Coord, dir: Dir, extent: usize) ?Coord {
+    return switch (dir) {
+        .North => if (c.y > 0) Coord{ .x = c.x, .y = c.y - 1 } else null,
+        .East => if (c.x + 1 < extent) Coord{ .x = c.x + 1, .y = c.y } else null,
+        .South => if (c.y + 1 < extent) Coord{ .x = c.x, .y = c.y + 1 } else null,
+        .West => if (c.x > 0) Coord{ .x = c.x - 1, .y = c.y } else null,
+    };
 }
 
-const Dir = enum { North, South, East, West };
-
 pub fn part1(this: *const @This()) !?i64 {
-    var visited = std.AutoArrayHashMap(Coord, void).init(this.allocator);
+    var rowsIter = mem.tokenizeScalar(u8, this.input, '\n');
+    const extent = mem.indexOfScalar(u8, this.input, '\n') orelse return error.NoNewline;
+
+    const grid_size = extent * extent;
+    var visited = try std.DynamicBitSet.initEmpty(this.allocator, grid_size);
     defer visited.deinit();
-    var map = std.AutoArrayHashMap(Coord, u8).init(this.allocator);
-    defer map.deinit();
 
-    var rowsIter = std.mem.tokenizeScalar(u8, this.input, '\n');
-
-    const extent = std.mem.indexOfScalar(u8, this.input, '\n') orelse return error.NoNewline;
+    var obstacles = try std.DynamicBitSet.initEmpty(this.allocator, grid_size);
+    defer obstacles.deinit();
 
     var foundPos: ?Coord = null;
-    var guardDir: Dir = .North;
-
     var y: usize = 0;
+
     while (rowsIter.next()) |row| {
         for (row, 0..) |c, x| {
             if (c == '#') {
-                try map.putNoClobber(.{ .x = x, .y = y }, c);
+                obstacles.set(y * extent + x);
             } else if (c == '^') {
                 foundPos = .{ .x = x, .y = y };
             }
@@ -62,47 +53,60 @@ pub fn part1(this: *const @This()) !?i64 {
     }
 
     var guardPos = foundPos orelse return error.NoGuardInInput;
-    try visited.put(guardPos, {});
+    var guardDir: Dir = .North;
+
+    visited.set(guardPos.y * extent + guardPos.x);
+    var count: i64 = 1;
 
     while (move(guardPos, guardDir, extent)) |next| {
-        if (map.get(next)) |_| {
-            guardDir = switch (guardDir) {
-                .North => .East,
-                .East => .South,
-                .South => .West,
-                .West => .North,
-            };
+        if (obstacles.isSet(next.y * extent + next.x)) {
+            guardDir = guardDir.turnRight();
         } else {
             guardPos = next;
-            try visited.put(next, {});
+            const idx = guardPos.y * extent + guardPos.x;
+            if (!visited.isSet(idx)) {
+                visited.set(idx);
+                count += 1;
+            }
         }
     }
 
-    return @as(i64, @intCast(visited.count()));
+    return count;
 }
 
-fn detect_loop(map: std.AutoArrayHashMap(Coord, void), initialGuardPos: Coord, extent: usize, visited: *std.AutoArrayHashMap(Coord, Dir)) !bool {
-    var guardDir: Dir = .North;
-    var guardPos: Coord = initialGuardPos;
+fn detectLoopFast(
+    obstacles: *std.DynamicBitSet,
+    initialPos: Coord,
+    extent: usize,
+    visited_states: []u4,
+) bool {
+    @memset(visited_states, 0);
 
-    try visited.put(guardPos, guardDir);
+    var pos = initialPos;
+    var dir: Dir = .North;
 
-    while (move(guardPos, guardDir, extent)) |next| {
-        if (visited.get(next)) |prevDir| {
-            if (prevDir == guardDir) {
+    const dir_bit: u4 = @as(u4, 1) << @intFromEnum(dir);
+    const idx = pos.y * extent + pos.x;
+    visited_states[idx] = dir_bit;
+
+    var steps: usize = 0;
+    const max_steps = extent * extent * 4;
+
+    while (move(pos, dir, extent)) |next| {
+        steps += 1;
+        if (steps > max_steps) return true;
+
+        if (obstacles.isSet(next.y * extent + next.x)) {
+            dir = dir.turnRight();
+        } else {
+            pos = next;
+            const next_idx = pos.y * extent + pos.x;
+            const next_dir_bit: u4 = @as(u4, 1) << @intFromEnum(dir);
+
+            if ((visited_states[next_idx] & next_dir_bit) != 0) {
                 return true;
             }
-        }
-        if (map.get(next)) |_| {
-            guardDir = switch (guardDir) {
-                .North => .East,
-                .East => .South,
-                .South => .West,
-                .West => .North,
-            };
-        } else {
-            guardPos = next;
-            try visited.put(next, guardDir);
+            visited_states[next_idx] |= next_dir_bit;
         }
     }
 
@@ -110,71 +114,68 @@ fn detect_loop(map: std.AutoArrayHashMap(Coord, void), initialGuardPos: Coord, e
 }
 
 pub fn part2(this: *const @This()) !?i64 {
-    var map = std.AutoArrayHashMap(Coord, void).init(this.allocator);
+    var rowsIter = mem.tokenizeScalar(u8, this.input, '\n');
+    const extent = mem.indexOfScalar(u8, this.input, '\n') orelse return error.NoNewline;
+    const grid_size = extent * extent;
 
-    var rowsIter = std.mem.tokenizeScalar(u8, this.input, '\n');
-
-    const extent = std.mem.indexOfScalar(u8, this.input, '\n') orelse return error.NoNewline;
+    var obstacles = try std.DynamicBitSet.initEmpty(this.allocator, grid_size);
+    defer obstacles.deinit();
 
     var foundPos: ?Coord = null;
-    {
-        var y: usize = 0;
-        while (rowsIter.next()) |row| {
-            for (row, 0..) |c, x| {
-                if (c == '#') {
-                    try map.putNoClobber(.{ .x = x, .y = y }, {});
-                } else if (c == '^') {
-                    foundPos = .{ .x = x, .y = y };
-                }
+    var y: usize = 0;
+
+    while (rowsIter.next()) |row| {
+        for (row, 0..) |c, x| {
+            if (c == '#') {
+                obstacles.set(y * extent + x);
+            } else if (c == '^') {
+                foundPos = .{ .x = x, .y = y };
             }
-            y += 1;
         }
+        y += 1;
     }
 
-    const initialGuardPos = foundPos orelse return error.NoGuardInInput;
-    var visited = std.AutoArrayHashMap(Coord, void).init(this.allocator);
-    defer visited.deinit();
-    var local_visited = std.AutoArrayHashMap(Coord, Dir).init(this.allocator);
-    defer local_visited.deinit();
+    const initialPos = foundPos orelse return error.NoGuardInInput;
+
+    var path_positions = try std.DynamicBitSet.initEmpty(this.allocator, grid_size);
+    defer path_positions.deinit();
 
     {
-        var guardPos = initialGuardPos;
-        var guardDir: Dir = .North;
-        try visited.put(guardPos, {});
+        var pos = initialPos;
+        var dir: Dir = .North;
+        path_positions.set(pos.y * extent + pos.x);
 
-        while (move(guardPos, guardDir, extent)) |next| {
-            if (map.get(next)) |_| {
-                guardDir = switch (guardDir) {
-                    .North => .East,
-                    .East => .South,
-                    .South => .West,
-                    .West => .North,
-                };
+        while (move(pos, dir, extent)) |next| {
+            if (obstacles.isSet(next.y * extent + next.x)) {
+                dir = dir.turnRight();
             } else {
-                guardPos = next;
-                try visited.put(next, {});
+                pos = next;
+                path_positions.set(pos.y * extent + pos.x);
             }
         }
     }
+
+    const visited_states = try this.allocator.alloc(u4, grid_size);
+    defer this.allocator.free(visited_states);
 
     var sum: i64 = 0;
-    var iter = visited.iterator();
 
-    while (iter.next()) |entry| {
-        const pos = entry.key_ptr;
-        if (initialGuardPos.x == pos.x and initialGuardPos.y == pos.y) {
-            continue;
-        } else if (map.get(pos.*)) |_| {
-            continue;
-        } else {
-            try map.putNoClobber(pos.*, {});
-            defer _ = map.swapRemove(pos.*);
-            defer local_visited.clearRetainingCapacity();
+    var iter = path_positions.iterator(.{});
+    while (iter.next()) |idx| {
+        const test_x = idx % extent;
+        const test_y = idx / extent;
 
-            if (try detect_loop(map, initialGuardPos, extent, &local_visited)) {
-                sum += 1;
-            }
+        if (test_x == initialPos.x and test_y == initialPos.y) continue;
+
+        if (obstacles.isSet(idx)) continue;
+
+        obstacles.set(idx);
+
+        if (detectLoopFast(&obstacles, initialPos, extent, visited_states)) {
+            sum += 1;
         }
+
+        obstacles.unset(idx);
     }
 
     return sum;
@@ -201,5 +202,5 @@ test "example" {
     };
 
     try std.testing.expectEqual(41, try problem.part1());
-    try std.testing.expectEqual(null, try problem.part2());
+    try std.testing.expectEqual(6, try problem.part2());
 }
