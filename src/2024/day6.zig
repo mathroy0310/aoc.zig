d84179d7da92c1e92edbf5e0da8bb010bfc8aa52
mathroy0310 <mathroy0310@gmail.com>
2025-11-16 21:48:37 -0500

    optimized 2024 day 6 even more

--- src/2024/day6.zig
+++ src/2024/day6.zig
@@ -4,178 +4,188 @@ const mem = std.mem;
 input: []const u8,
 allocator: mem.Allocator,
 
-const Coord = struct { x: usize, y: usize };
-
-const Dir = enum(u2) {
-    North = 0,
-    East = 1,
-    South = 2,
-    West = 3,
-
-    fn turnRight(self: Dir) Dir {
-        const next: u2 = @truncate((@as(u3, @intFromEnum(self)) + 1) % 4);
-        return @enumFromInt(next);
-    }
-};
-
-fn move(c: Coord, dir: Dir, extent: usize) ?Coord {
+inline fn move(x: u16, y: u16, dir: u2, extent: u16) ?struct { x: u16, y: u16 } {
     return switch (dir) {
-        .North => if (c.y > 0) Coord{ .x = c.x, .y = c.y - 1 } else null,
-        .East => if (c.x + 1 < extent) Coord{ .x = c.x + 1, .y = c.y } else null,
-        .South => if (c.y + 1 < extent) Coord{ .x = c.x, .y = c.y + 1 } else null,
-        .West => if (c.x > 0) Coord{ .x = c.x - 1, .y = c.y } else null,
+        0 => if (y > 0) .{ .x = x, .y = y - 1 } else null, // North
+        1 => if (x + 1 < extent) .{ .x = x + 1, .y = y } else null, // East
+        2 => if (y + 1 < extent) .{ .x = x, .y = y + 1 } else null, // South
+        3 => if (x > 0) .{ .x = x - 1, .y = y } else null, // West
     };
 }
 
 pub fn part1(this: *const @This()) !?i64 {
-    var rowsIter = mem.tokenizeScalar(u8, this.input, '\n');
-    const extent = mem.indexOfScalar(u8, this.input, '\n') orelse return error.NoNewline;
+    const extent: u16 = @intCast(mem.indexOfScalar(u8, this.input, '\n') orelse return error.NoNewline);
+    const grid_size = @as(usize, extent) * extent;
 
-    const grid_size = extent * extent;
-    var visited = try std.DynamicBitSet.initEmpty(this.allocator, grid_size);
-    defer visited.deinit();
+    var obstacles = try this.allocator.alloc(bool, grid_size);
+    defer this.allocator.free(obstacles);
+    @memset(obstacles, false);
 
-    var obstacles = try std.DynamicBitSet.initEmpty(this.allocator, grid_size);
-    defer obstacles.deinit();
+    var visited = try this.allocator.alloc(bool, grid_size);
+    defer this.allocator.free(visited);
+    @memset(visited, false);
 
-    var foundPos: ?Coord = null;
-    var y: usize = 0;
+    var pos_x: u16 = undefined;
+    var pos_y: u16 = undefined;
+    var parse_y: u16 = 0;
+    var idx: usize = 0;
 
-    while (rowsIter.next()) |row| {
-        for (row, 0..) |c, x| {
-            if (c == '#') {
-                obstacles.set(y * extent + x);
-            } else if (c == '^') {
-                foundPos = .{ .x = x, .y = y };
-            }
+    while (idx < this.input.len) {
+        const c = this.input[idx];
+        if (c == '\n') {
+            parse_y += 1;
+            idx += 1;
+            continue;
         }
-        y += 1;
+
+        const x: u16 = @intCast(idx - parse_y * (extent + 1));
+        const grid_idx = @as(usize, parse_y) * extent + x;
+
+        if (c == '#') {
+            obstacles[grid_idx] = true;
+        } else if (c == '^') {
+            pos_x = x;
+            pos_y = parse_y;
+        }
+
+        idx += 1;
     }
 
-    var guardPos = foundPos orelse return error.NoGuardInInput;
-    var guardDir: Dir = .North;
+    var dir: u2 = 0;
+    var count: i64 = 0;
 
-    visited.set(guardPos.y * extent + guardPos.x);
-    var count: i64 = 1;
+    while (true) {
+        const idx_curr = @as(usize, pos_y) * extent + pos_x;
+        if (!visited[idx_curr]) {
+            visited[idx_curr] = true;
+            count += 1;
+        }
 
-    while (move(guardPos, guardDir, extent)) |next| {
-        if (obstacles.isSet(next.y * extent + next.x)) {
-            guardDir = guardDir.turnRight();
+        const next = move(pos_x, pos_y, dir, extent) orelse break;
+        const next_idx = @as(usize, next.y) * extent + next.x;
+
+        if (obstacles[next_idx]) {
+            dir = @as(u2, @intCast((@as(u8, dir) + 1) & 3));
         } else {
-            guardPos = next;
-            const idx = guardPos.y * extent + guardPos.x;
-            if (!visited.isSet(idx)) {
-                visited.set(idx);
-                count += 1;
-            }
+            pos_x = next.x;
+            pos_y = next.y;
         }
     }
 
     return count;
 }
 
-fn detectLoopFast(
-    obstacles: *std.DynamicBitSet,
-    initialPos: Coord,
-    extent: usize,
-    visited_states: []u4,
+fn detectLoop(
+    obstacles: []bool,
+    start_x: u16,
+    start_y: u16,
+    extent: u16,
+    visited_states: []u8,
 ) bool {
     @memset(visited_states, 0);
 
-    var pos = initialPos;
-    var dir: Dir = .North;
+    var x = start_x;
+    var y = start_y;
+    var dir: u2 = 0;
 
-    const dir_bit: u4 = @as(u4, 1) << @intFromEnum(dir);
-    const idx = pos.y * extent + pos.x;
-    visited_states[idx] = dir_bit;
+    while (true) {
+        const idx = @as(usize, y) * extent + x;
+        const dir_mask: u8 = @as(u8, 1) << dir;
 
-    var steps: usize = 0;
-    const max_steps = extent * extent * 4;
+        if ((visited_states[idx] & dir_mask) != 0) {
+            return true;
+        }
+        visited_states[idx] |= dir_mask;
 
-    while (move(pos, dir, extent)) |next| {
-        steps += 1;
-        if (steps > max_steps) return true;
+        const next = move(x, y, dir, extent) orelse return false;
+        const next_idx = @as(usize, next.y) * extent + next.x;
 
-        if (obstacles.isSet(next.y * extent + next.x)) {
-            dir = dir.turnRight();
+        if (obstacles[next_idx]) {
+            dir = @as(u2, @intCast((@as(u8, dir) + 1) & 3));
         } else {
-            pos = next;
-            const next_idx = pos.y * extent + pos.x;
-            const next_dir_bit: u4 = @as(u4, 1) << @intFromEnum(dir);
-
-            if ((visited_states[next_idx] & next_dir_bit) != 0) {
-                return true;
-            }
-            visited_states[next_idx] |= next_dir_bit;
+            x = next.x;
+            y = next.y;
         }
     }
-
-    return false;
 }
 
 pub fn part2(this: *const @This()) !?i64 {
-    var rowsIter = mem.tokenizeScalar(u8, this.input, '\n');
-    const extent = mem.indexOfScalar(u8, this.input, '\n') orelse return error.NoNewline;
-    const grid_size = extent * extent;
+    const extent: u16 = @intCast(mem.indexOfScalar(u8, this.input, '\n') orelse return error.NoNewline);
+    const grid_size = @as(usize, extent) * extent;
 
-    var obstacles = try std.DynamicBitSet.initEmpty(this.allocator, grid_size);
-    defer obstacles.deinit();
+    var obstacles = try this.allocator.alloc(bool, grid_size);
+    defer this.allocator.free(obstacles);
+    @memset(obstacles, false);
 
-    var foundPos: ?Coord = null;
-    var y: usize = 0;
+    var path = try this.allocator.alloc(bool, grid_size);
+    defer this.allocator.free(path);
+    @memset(path, false);
 
-    while (rowsIter.next()) |row| {
-        for (row, 0..) |c, x| {
-            if (c == '#') {
-                obstacles.set(y * extent + x);
-            } else if (c == '^') {
-                foundPos = .{ .x = x, .y = y };
-            }
-        }
-        y += 1;
-    }
-
-    const initialPos = foundPos orelse return error.NoGuardInInput;
-
-    var path_positions = try std.DynamicBitSet.initEmpty(this.allocator, grid_size);
-    defer path_positions.deinit();
-
-    {
-        var pos = initialPos;
-        var dir: Dir = .North;
-        path_positions.set(pos.y * extent + pos.x);
-
-        while (move(pos, dir, extent)) |next| {
-            if (obstacles.isSet(next.y * extent + next.x)) {
-                dir = dir.turnRight();
-            } else {
-                pos = next;
-                path_positions.set(pos.y * extent + pos.x);
-            }
-        }
-    }
-
-    const visited_states = try this.allocator.alloc(u4, grid_size);
+    const visited_states = try this.allocator.alloc(u8, grid_size);
     defer this.allocator.free(visited_states);
 
+    var initial_x: u16 = undefined;
+    var initial_y: u16 = undefined;
+    var parse_y: u16 = 0;
+    var idx: usize = 0;
+
+    while (idx < this.input.len) {
+        const c = this.input[idx];
+        if (c == '\n') {
+            parse_y += 1;
+            idx += 1;
+            continue;
+        }
+
+        const px: u16 = @intCast(idx - parse_y * (extent + 1));
+        const grid_idx = @as(usize, parse_y) * extent + px;
+
+        if (c == '#') {
+            obstacles[grid_idx] = true;
+        } else if (c == '^') {
+            initial_x = px;
+            initial_y = parse_y;
+        }
+
+        idx += 1;
+    }
+
+    // Trace original path
+    {
+        var x = initial_x;
+        var y = initial_y;
+        var dir: u2 = 0;
+        path[@as(usize, y) * extent + x] = true;
+
+        while (move(x, y, dir, extent)) |next| {
+            const next_idx = @as(usize, next.y) * extent + next.x;
+
+            if (obstacles[next_idx]) {
+                dir = @as(u2, @intCast((@as(u8, dir) + 1) & 3));
+            } else {
+                x = next.x;
+                y = next.y;
+                path[next_idx] = true;
+            }
+        }
+    }
+
     var sum: i64 = 0;
+    const initial_idx = @as(usize, initial_y) * extent + initial_x;
 
-    var iter = path_positions.iterator(.{});
-    while (iter.next()) |idx| {
-        const test_x = idx % extent;
-        const test_y = idx / extent;
+    var test_idx: usize = 0;
+    while (test_idx < grid_size) : (test_idx += 1) {
+        if (!path[test_idx] or test_idx == initial_idx or obstacles[test_idx]) {
+            continue;
+        }
 
-        if (test_x == initialPos.x and test_y == initialPos.y) continue;
+        obstacles[test_idx] = true;
 
-        if (obstacles.isSet(idx)) continue;
-
-        obstacles.set(idx);
-
-        if (detectLoopFast(&obstacles, initialPos, extent, visited_states)) {
+        if (detectLoop(obstacles, initial_x, initial_y, extent, visited_states)) {
             sum += 1;
         }
 
-        obstacles.unset(idx);
+        obstacles[test_idx] = false;
     }
 
     return sum;
